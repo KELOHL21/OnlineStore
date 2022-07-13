0472d4ee6230d70d1cc1423e64592181e39c32c8
KELOHL21 <kelsyohlsson@gmail.com>
2022-07-14 00:02:46 +0200

    Updates

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+// We've created an index.php file
+// In the following two lines, we are calling the two dependacies within the Slim which we are going to use of later 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require './vendor/autoload.php';// This is compulsory for our framework to work, but don't worry too much about how it works
+
+$app = new \Slim\App;// We create an object of the Slim framework main app
+
+var_dump($app);
